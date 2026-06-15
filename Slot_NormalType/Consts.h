2bf98d9a625b5104ab95cf09fa2fbc6431d46acf
C4164 <c4164@oic.jp>
2026-06-15 13:55:22 +0900

    小役の確率を定義
    ・値は未定なので仮置き
    ・６段階の設定によって確率を定義
    ・16bit(65536)分の何かを整数で定義

--- Slot_NormalType/Consts.h
+++ Slot_NormalType/Consts.h
@@ -1,6 +1,21 @@
 #pragma once
 
-namespace Const 
+namespace Const
 {
+	//設定の段階(数)
+	constexpr int SETTING = 6;
 
+	//設定別の小役確率[分子：16bit(65536)分の何か]
+	struct Probability
+	{
+		const int BELL_1[SETTING] = { 0,0,0,0,0,0 };			//ベル１
+		const int BELL_2[SETTING] = { 0,0,0,0,0,0 };			//ベル２
+		const int NAVIGATED_BELL[SETTING] = { 0,0,0,0,0,0 };	//押し順ベル
+		const int REPLAY_1[SETTING] = { 0,0,0,0,0,0 };			//リプレイ１
+		const int REPLAY_2[SETTING] = { 0,0,0,0,0,0 };			//リプレイ２
+		const int MELON_WEAKNESS[SETTING] = { 0,0,0,0,0,0 };	//弱スイカ
+		const int MELON_STRENGTH[SETTING] = { 0,0,0,0,0,0 };	//強スイカ
+		const int CHERRY_WEAKNESS[SETTING] = { 0,0,0,0,0,0 };	//弱チェリー
+		const int CHERRY_STRENGTH[SETTING] = { 0,0,0,0,0,0 };	//強チェリー
+	};
 }
