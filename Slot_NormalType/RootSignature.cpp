70107a0cff81024a8d0ddd1b60f51690534f4985
C4164 <c4164@oic.jp>
2026-06-18 14:54:32 +0900

    RootSignatureクラスの作成とHLSLファイルの作成
    それぞれ中身はまだ空

---
+++ Slot_NormalType/RootSignature.cpp
@@ -0,0 +1,2 @@
+#include "RootSignature.h"
+
---
+++ Slot_NormalType/RootSignature.h
@@ -0,0 +1,11 @@
+#pragma once
+#pragma once
+#include <d3d12.h>
+#include "d3dx12.h"
+#include <d3dcompiler.h>
+#include <wrl.h>
+
+class RootSignature 
+{
+
+};
