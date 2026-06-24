644da5a21dfc36987ec517f9038357af7a5f30ab
C4164 <c4164@oic.jp>
2026-06-24 17:12:52 +0900

    ルートシグネチャクラスを作成

--- Slot_NormalType/RootSignature.cpp
+++ Slot_NormalType/RootSignature.cpp
@@ -1,2 +1,81 @@
 #include "RootSignature.h"
+#include <d3d12.h>
+#include <d3d12shader.h>
+#include <d3dcompiler.h>
+
+bool RootSignature::Init(ID3D12Device* device)
+{
+    //CBV(定数バッファ)
+    D3D12_DESCRIPTOR_RANGE cbvRange = {};
+    cbvRange.RangeType = D3D12_DESCRIPTOR_RANGE_TYPE_CBV;
+    cbvRange.NumDescriptors = 1;
+    cbvRange.BaseShaderRegister = 0;
+    cbvRange.RegisterSpace = 0;
+    cbvRange.OffsetInDescriptorsFromTableStart = D3D12_DESCRIPTOR_RANGE_OFFSET_APPEND;
+
+    //SRV(テクスチャー)
+    D3D12_DESCRIPTOR_RANGE srvRange = {};
+    srvRange.RangeType = D3D12_DESCRIPTOR_RANGE_TYPE_SRV;
+    srvRange.NumDescriptors = 1;
+    srvRange.BaseShaderRegister = 0;
+    srvRange.RegisterSpace = 0;
+    srvRange.OffsetInDescriptorsFromTableStart = D3D12_DESCRIPTOR_RANGE_OFFSET_APPEND;
+
+    //ルートパラメータ(CBV+SRV)
+    D3D12_ROOT_PARAMETER rootParams[2] = {};
+
+    //CBV用のディスクリプタテーブル
+    rootParams[0].ParameterType = D3D12_ROOT_PARAMETER_TYPE_DESCRIPTOR_TABLE;
+    rootParams[0].ShaderVisibility = D3D12_SHADER_VISIBILITY_ALL;
+    rootParams[0].DescriptorTable.NumDescriptorRanges = 1;
+    rootParams[0].DescriptorTable.pDescriptorRanges = &cbvRange;
+
+    //SRV用のディスクリプタテーブル
+    rootParams[1].ParameterType = D3D12_ROOT_PARAMETER_TYPE_DESCRIPTOR_TABLE;
+    rootParams[1].ShaderVisibility = D3D12_SHADER_VISIBILITY_ALL;
+    rootParams[1].DescriptorTable.NumDescriptorRanges = 1;
+    rootParams[1].DescriptorTable.pDescriptorRanges = &srvRange;
+
+    //サンプラー
+    D3D12_STATIC_SAMPLER_DESC sampler = {};
+    sampler.Filter = D3D12_FILTER_MIN_MAG_MIP_LINEAR;
+    sampler.AddressU = D3D12_TEXTURE_ADDRESS_MODE_WRAP;
+    sampler.AddressV = D3D12_TEXTURE_ADDRESS_MODE_WRAP;
+    sampler.AddressW = D3D12_TEXTURE_ADDRESS_MODE_WRAP;
+    sampler.ShaderVisibility = D3D12_SHADER_VISIBILITY_PIXEL;
+    sampler.ShaderRegister = 0;
+
+    //ルートシグネチャの情報をまとめる構造体
+    D3D12_ROOT_SIGNATURE_DESC desc = {};
+    desc.NumParameters = _countof(rootParams);
+    desc.pParameters = rootParams;
+    desc.NumStaticSamplers = 1;
+    desc.pStaticSamplers = &sampler;
+
+	//バイナリ形式(二進数や生データ)に変換するためのバッファ
+	ComPtr<ID3DBlob> signatureBlob;
+	ComPtr<ID3DBlob> errorBlob;
+
+	//バイナリ形式に変換
+	HRESULT hr = D3D12SerializeRootSignature(
+		&desc,
+		D3D_ROOT_SIGNATURE_VERSION_1,
+		&signatureBlob,
+		&errorBlob
+	);
+
+	//失敗したらfalseを返して終了
+	if (FAILED(hr)) { return false; }
+
+	//ルートシグネチャを作成
+	hr = device->CreateRootSignature(
+		0,
+		signatureBlob->GetBufferPointer(),
+		signatureBlob->GetBufferSize(),
+		IID_PPV_ARGS(&rootSignature)
+	);
+
+	return SUCCEEDED(hr);
+}
+
 
--- Slot_NormalType/RootSignature.h
+++ Slot_NormalType/RootSignature.h
@@ -1,11 +1,21 @@
 #pragma once
-#pragma once
 #include <d3d12.h>
-#include "d3dx12.h"
-#include <d3dcompiler.h>
 #include <wrl.h>
+#include "ComPtr.h"
 
-class RootSignature 
+class RootSignature
 {
+public:
+    //コンストラクタとデストラクタ
+    RootSignature() = default;
+    ~RootSignature() = default;
 
+    //初期化関数
+    bool Init(ID3D12Device* device);
+
+    //ゲッター
+    ID3D12RootSignature* Get() const { return rootSignature.Get(); }
+
+private:
+    ComPtr<ID3D12RootSignature> rootSignature;
 };
